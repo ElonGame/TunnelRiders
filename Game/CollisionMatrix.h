8bec1566a8c675dc825a08c813ca12b750682005
Sam Murphy <sam-murphy@hotmail.co.uk>
2017-05-27 00:38:06 +0100

    Fixed bit mask numbering
    need to be powers of 2

--- Game/CollisionMatrix.h
+++ Game/CollisionMatrix.h
@@ -1,10 +1,10 @@
 #pragma once
 
-
+// Need to be powers of 2, for bit mask to work
 enum CollisionID
 {
 	Ship_ID = 1,
 	Tunnel_ID = 2,
-	Obstacle_ID = 3,
-	Power_Up = 4
+	Obstacle_ID = 4,
+	Power_Up = 8
 };
